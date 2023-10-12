df15cc788cbae9ba20d395aed1717751816e6c67
Reza Sajadi <reza.sajadee@gmail.com>
2023-10-12 14:16:54 +0000

    Finish Serializers

--- blango/settings.py
+++ blango/settings.py
@@ -59,6 +59,7 @@ class Dev(Configuration):
       "allauth.account",
       "allauth.socialaccount",
       "allauth.socialaccount.providers.google",
+      "rest_framework",
 
   ]
   INTERNAL_IPS = ["192.168.11.179"]
