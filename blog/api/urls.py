eaecc897da3f19e8238ff0f417712b390312fb08
Reza Sajadi <reza.sajadee@gmail.com>
2024-03-21 17:47:15 +0000

    Finish simplejwt

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path,include
 from rest_framework.urlpatterns import format_suffix_patterns
 
 from rest_framework.authtoken import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from blog.api.views import UserDetail
 from rest_framework.routers import DefaultRouter
@@ -23,7 +24,9 @@ urlpatterns = format_suffix_patterns(urlpatterns)
 
 urlpatterns += [
     path("auth/", include("rest_framework.urls")),
-    path("token-auth/", views.obtain_auth_token)
+    path("token-auth/", views.obtain_auth_token),
+    path("jwt/", TokenObtainPairView.as_view(), name="jwt_obtain_pair"),
+    path("jwt/refresh/", TokenRefreshView.as_view(), name="jwt_refresh"),
 ]
 
 schema_view = get_schema_view(
